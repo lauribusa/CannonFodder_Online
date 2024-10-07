9b6d3fed6c111b8bb3cfcff65a682c09c33525b0
Breniak <kyllian_bocklandt@hotmail.com>
2024-10-07 17:33:32 +0200

    add cannon prefab

---
+++ Assets/Features/Controllers/CannonController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    [SerializeField] private Transform _rotator;
+    [SerializeField] private Transform _elevator;
+
+    public void RotateCannon(float degrees)
+    {
+        
+    }
+}
