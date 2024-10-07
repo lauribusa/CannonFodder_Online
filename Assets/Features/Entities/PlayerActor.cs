09b07c332baf5a4bbcc7c4309af8f968b7b21bd0
LB <lauri.busa@gmail.com>
2024-10-07 15:11:37 +0200

    first network test

--- Assets/Features/Entities/PlayerActor.cs
+++ Assets/Features/Entities/PlayerActor.cs
@@ -1,10 +1,10 @@
 ﻿using Assets.Features.Fragments.ScriptableObjectVariables;
+using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace Assets.Features.Entities
 {
-    public class PlayerActor : MonoBehaviour
+    public class PlayerActor : NetworkBehaviour
     {
         [SerializeField]
         private bool debug;
@@ -26,21 +26,23 @@ namespace Assets.Features.Entities
         {
             if (carriedItem == null)
             {
-                PerformPickup();
+                PerformPickupRpc();
                 return;
             }
 
-            PerformPutDown();
+            PerformPutDownRpc();
 
         }
 
-        private void SetItem()
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SetItemParentRpc()
         {
             carriedItem.transform.SetParent(itemAnchorPoint);
-            carriedItem.transform.localPosition = Vector3.zero;
+            carriedItem = carriedItem.PickUp();
         }
 
-        private void PerformPickup()
+        [Rpc(SendTo.Server)]
+        private void PerformPickupRpc()
         {
             if (debug) Debug.Log($"{carriableItemsInScene.GetList().Count} items in scene", gameObject);
             var existingItems = carriableItemsInScene.GetList();
@@ -49,20 +51,18 @@ namespace Assets.Features.Entities
             {
                 if (item.isCarried.Value) continue;
                 var distance = Vector3.Distance(transform.position, item.transform.position);
-                if (debug) Debug.Log($"Distance between player and {item.name}: {distance} (required: {GameHelpers.DetectionRange})", gameObject);
+                if (debug) Debug.Log($"Distance between {gameObject.name} and {item.name}: {distance} (required: {GameHelpers.DetectionRange})", gameObject);
                 if (distance <= GameHelpers.DetectionRange)
                 {
-                    carriedItem = item.PickUp();
-                    carriedItem.transform.SetParent(itemAnchorPoint);
-                    carriedItem.transform.localPosition = Vector3.zero;
-                    
-                    if(debug) Debug.Log($"Carrying {item.name}", gameObject);
+                    SetItemParentRpc();
+                    if (debug) Debug.Log($"Carrying {item.name}", gameObject);
                     break;
                 }
             }
         }
 
-        private void PerformPutDown()
+        [Rpc(SendTo.Server)]
+        private void PerformPutDownRpc()
         {
             if (debug) Debug.Log($"Putting {carriedItem.name} down.", gameObject);
             carriedItem.transform.SetParent(null);
---
+++ Assets/Features/Entities/PlayerNetwork.cs
@@ -0,0 +1,62 @@
+using System;
+using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    private NetworkVariable<Color> playerColor = new();
+    private NetworkVariable<FixedString128Bytes> playerName = new(writePerm: NetworkVariableWritePermission.Owner);
+    private TMP_Text _tag;
+    public override void OnNetworkSpawn()
+    {
+        playerColor.OnValueChanged += OnColorChange;
+        playerName.OnValueChanged += OnTextChange;
+
+        if (IsLocalPlayer)
+        {
+            GetPositionRpc();
+            playerName.Value = Guid.NewGuid().ToString();
+        }
+        if (IsServer)
+        {
+            playerColor.Value = UnityEngine.Random.ColorHSV(0, 1, 1, 1, 1, 1);
+        }
+        OnTextChange(playerName.Value, playerName.Value);
+        OnColorChange(playerColor.Value, playerColor.Value);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        playerColor.OnValueChanged -= OnColorChange;
+        playerName.OnValueChanged -= OnTextChange;
+    }
+
+    private void OnTextChange(FixedString128Bytes previous, FixedString128Bytes newValue)
+    {
+        gameObject.name = newValue.ToString();
+        _tag.SetText(newValue.ToString());
+    }
+
+    private void OnColorChange(Color prev, Color next)
+    {
+        GetComponent<MeshRenderer>().material.color = next;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void GetPositionRpc()
+    {
+        var playerPosition = new Vector3(UnityEngine.Random.Range(transform.position.x - 1.5f, transform.position.x + 1.5f), transform.position.y, UnityEngine.Random.Range(transform.position.z - 1.5f, transform.position.z + 1.5f));
+
+        if (!IsHost) transform.position = playerPosition;
+
+        SetPositionRpc(playerPosition);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void SetPositionRpc(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
