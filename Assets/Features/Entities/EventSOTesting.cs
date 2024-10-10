2b446e83b661bfa18a30c66efab989095ec59dea
ChaosBreniak <kyllian_bocklandt@hotmail.com>
2024-10-10 15:47:41 +0200

    add logic to fire the cannon

---
+++ Assets/Features/Entities/EventSOTesting.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Features.Fragments.ScriptableObjectEvents;
+using Unity.Netcode;
+using UnityEngine;
+
+public class EventSOTesting : NetworkBehaviour
+{
+    [SerializeField] private VoidEventSO _onCannonFired;
+
+    private void OnEnable() => _onCannonFired.Subscribe(OnCannonFired);
+
+    private void OnDisable() => _onCannonFired.Unsubscribe(OnCannonFired);
+
+    private void OnCannonFired() => OnCannonFiredClientRpc();
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void OnCannonFiredClientRpc()
+    {
+        Debug.Log("Cannon has fired");
+    }
+}
