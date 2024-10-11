db62ca84911c49cab2b49f8323cce311346f20ef
LB <lauri.busa@gmail.com>
2024-10-11 11:49:18 +0200

    added cinemachine with screenshake + sfx on score

---
+++ Assets/Features/Systems/AudioBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Features.Fragments.ScriptableObjectEvents;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Assets.Features.Systems
+{
+    public class AudioBehavior: MonoBehaviour
+    {
+        public CinemachineImpulseSource impulseSource; 
+        public AudioSource audioSource;
+        public VoidEventSO onScore;
+        public AudioClip fireSound;
+        private void OnEnable()
+        {
+            onScore.Subscribe(PlaySound);
+        }
+
+        private void OnDisable()
+        {
+            onScore.Unsubscribe(PlaySound);
+        }
+
+        private void PlaySound()
+        {
+            audioSource.PlayOneShot(fireSound);
+            impulseSource.GenerateImpulse(5f);
+        }
+    }
+}
